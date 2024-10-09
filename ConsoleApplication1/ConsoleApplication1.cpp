13b772e63a7a25cbd83eb014ad0492492c7c0045
FORMOZA 44-88-44 <FORMOZA 44-88-44@EVGEN>
2024-10-10 01:05:20 +0800

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool isPointInSquare(double x, double y)
+{
+	if (y >= -x - 1 & y <= -x + 1 & y <= x + 1 & y >= x - 1)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int main()
+{
+	double x;
+	double y;
+	cin >> x >> y;
+	(isPointInSquare(x, y)) ? (cout << "YES") : (cout << "NO");
+	return 0;
+}
